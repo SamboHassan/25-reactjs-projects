d5435ebe52edd292de84e524f09a322b450f1491
SamboHassan~ <sambohasan@gmail.com>
2023-08-12 22:55:04 +0100

    �

---
+++ use-effect-hooks/src/messageStyles.js
@@ -0,0 +1,20 @@
+export const warningStyle = {
+  color: 'red',
+  padding: '6px',
+  backgroundColor: '#000000',
+  fontSize: '25px',
+};
+
+export const infoStyle = {
+  color: 'yellow',
+  padding: '6px',
+  backgroundColor: '#000000',
+  fontSize: '25px',
+};
+
+export const successStyle = {
+  color: 'green',
+  padding: '6px',
+  backgroundColor: '#000000',
+  fontSize: '25px',
+};
