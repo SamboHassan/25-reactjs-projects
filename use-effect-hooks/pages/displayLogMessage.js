e7a081d39f3b6185c056effb83e23f5424f20dca
SamboHassan~ <sambohasan@gmail.com>
2023-08-12 22:52:15 +0100

    feat: �  add styling component exercise file

---
+++ use-effect-hooks/pages/displayLogMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DisplayStatus from '../src/displayStatus';
+
+const displayLogMessage = () => {
+  return (
+    <>
+      <DisplayStatus message="success" />
+    </>
+  );
+};
+
+export default displayLogMessage;
