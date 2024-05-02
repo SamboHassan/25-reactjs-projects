444ef869bca0e9fb654802e891bc864fa26496d2
Hassan Sambo <sambohasan@gmail.com>
2024-05-02 17:43:14 +0100

    add random color generator component

--- reactjs-projects/src/App.js
+++ reactjs-projects/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* Accordian component */}
+      {/* <Accordian /> */}
+
+      {/* RandomColor component */}
+      <RandomColor />
     </div>
   );
 }
